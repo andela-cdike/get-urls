0111d7ceca7e4e732da9e77f14213966d7b6dd1b
andela-cdike <chukwuerika.dike@andela.com>
2017-06-17 16:09:50 +0100

    Transpile to ES5

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const urlRegex = require('url-regex');
 const normalizeUrl = require('normalize-url');
 
-module.exports = (str, opts) => {
-	const urls = str.match(urlRegex());
-	const ret = urls ? urls.map(url => normalizeUrl(url.trim().replace(/\.+$/, ''), opts)) : [];
-	return new Set(ret);
+module.exports = function (str, opts) {
+  var urls = str.match(urlRegex());
+  var ret = urls ? urls.map(
+    function (url) {
+      return normalizeUrl(url.trim().replace(/\.+$/, ''), opts);
+    }) : [];
+  return new Set(ret);
 };
